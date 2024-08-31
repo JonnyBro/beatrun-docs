5c1d51b22c9192db2fd19377a35bf9b16f5c8253
Jonny_Bro (Nikita) <niktom.nikto@ya.ru>
2024-08-31 19:37:05 +0500

    Installing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,13 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function HomePage() {
-  return (
-    <main className="flex h-screen flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
-        You can open{' '}
-        <Link
-          href="/docs"
-          className="text-fd-foreground font-semibold underline"
-        >
-          /docs
-        </Link>{' '}
-        and see the documentation.
-      </p>
-    </main>
-  );
+	return (
+		<main className="flex h-screen flex-col justify-center text-center">
+			<h1 className="mb-4 text-2xl font-bold">Welcome to Beatrun Docs</h1>
+			<p className="text-fd-muted-foreground">
+				This is a simple docs site for Jonny's fork of Beatrun.<br></br>
+				You can open <Link href="/docs" className="text-fd-foreground font-semibold underline">/docs</Link> and see the documentation.
+			</p>
+		</main>
+	);
 }
