b8a7db09e3b147a2606277ef8254f339d0c2f72a
Jonny_Bro (Nikita) <niktom.nikto@ya.ru>
2024-09-03 20:22:43 +0500

    Starting with FAQ

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,7 @@ export default function HomePage() {
 		<main className="flex h-screen flex-col justify-center text-center">
 			<h1 className="mb-4 text-2xl font-bold">Welcome to Beatrun Docs</h1>
 			<p className="text-fd-muted-foreground">
-				This is a simple docs site for Jonny's fork of Beatrun.<br></br>
-				You can open <Link href="/docs" className="text-fd-foreground font-semibold underline">/docs</Link> and see the documentation.
+				<Link href="/docs" className="text-fd-foreground font-semibold underline">Go here</Link>
 			</p>
 		</main>
 	);
